32d23b970b52d22676091305828898725c9b3f84
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2022-11-11 16:41:17 -0300

    added a server to make api calls

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
-import { CloseButton, Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionTypeButton, TransactionTypeContainer } from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -19,6 +19,16 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionTypeContainer>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionTypeContainer>
           <button type="submit">
             Cadastrar
           </button>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
+
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function fetchTransactions() {
+    const response = await fetch('http://localhost/3333/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -13,18 +38,24 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td>
-              </td>
-              <td>
-                <PriceHighlight variant='income'>
-                </PriceHighlight>
-              </td>
-              <td>
-              </td>
-              <td>
-              </td>
-            </tr>
+            {transactions.length > 0 ? transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">
+                  {transaction.description}
+                </td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighlight>
+                </td>
+                <td>
+                  {transaction.category}
+                </td>
+                <td>
+                  {transaction.createdAt}
+                </td>
+              </tr>
+            )) : null}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
